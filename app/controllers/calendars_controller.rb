ca37acf249a56415dc8919f88d931bf35b80c481
daikikawahata <daikikawahata@icloud.com>
2020-08-17 17:13:48 +0900

    Revert "issue4"
    This reverts commit 4a215d8e90c72e0be098f8799aa51ac468ae9fe0.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,8 +34,9 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans}
       @week_days.push(days)
     end
+
   end
 end
